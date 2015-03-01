5484dd3c67731b2afeec74467771a92a3049918d
JENNIFER AMICO <jenniferamico@Jens-Mac.local>
2015-02-28 21:54:46 -0800

    messed with landing page and picked poet images

--- flask_poetry.py
+++ flask_poetry.py
@@ -1,6 +1,5 @@
-from random import choice
-from flask import Flask, render_template, request
 
+from flask import Flask, render_template
 
 # "__name__" is a special Python variable for the name of the current module; Flask wants
 # to know this to know what any imported things are relative to.
@@ -9,27 +8,15 @@ app = Flask(__name__)
 # route to handle the landing page of a website.
 @app.route('/')
 def start_here():
-    return "Hi! This is the magnetic poets home page."
+    return render_template("index.html")
 
 # route to display a simple web page
 @app.route('/pick_a_poet')
 def pick_a_poet():
     return render_template("pick_a_poet.html")
 
-@app.route('/greet')
-def greet_person():
-    player = request.args.get("person")
-
-    AWESOMENESS = [
-        'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
-        'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
-
-    compliment = choice(AWESOMENESS)
-
-    return render_template("compliment.html", person=player, compliment=compliment)
-
-
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
     # if we change the code.
     app.run(debug=True)
+
